e6aaa894853724a6dfbdfbad1c1a4a64b348a7b3
Hulk Hogan <thehulkhogan@yahoo.com>
2020-08-02 08:57:13 -0400

    Wikipedia sample api call and adding to state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Form from './components/Form'
 
 class App extends Component {
   state = {
-    characters: [
-    ],
+    characters: [],
+    data:[],
   }
   removeCharacter = (index) => {
     const { characters } = this.state;
@@ -17,11 +17,33 @@ class App extends Component {
     //setting state back for characters
     this.setState({ characters: filteredCharacters });
   }
+  // Code is invoked after the component is mounted/inserted into the DOM tree.
+  componentDidMount() {
+    const url =
+      'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
+      //'https://gateway.marvel.com:443/v1/public/characters?apikey=399be848cd869eff0ef2177690b0ef8a'
+
+    fetch(url)
+      .then((result) => result.json())
+      .then((result) => {
+        this.setState({
+          data: result,          
+        })
+      })
+  }
+
   handleSubmit = (character) => {
     this.setState({ characters: [...this.state.characters, character] })
   }
   render() {
     const { characters } = this.state;
+    const { data } = this.state;
+    //const result = [];
+    console.log(data);
+    const result = data.map((entry, index) => {
+      return <li key={index}>{entry}</li>
+    })
+    
     return (
       <div>
         <header></header>
@@ -30,6 +52,7 @@ class App extends Component {
           <div className="container">
             <Form handleSubmit={this.handleSubmit}></Form>
             <Table characterData={characters} removeCharacter={this.removeCharacter} />
+            <ul>{result}</ul>
           </div>
         </main>
         <footer></footer>
