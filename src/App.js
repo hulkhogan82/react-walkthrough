621f8f960e9595160bc3dcc581c40854ab4c963e
Hulk Hogan <thehulkhogan@yahoo.com>
2020-08-02 07:15:25 -0400

    Form and state, to add new character

--- src/App.js
+++ src/App.js
@@ -1,46 +1,35 @@
 import React, { Component } from 'react'
 import Table from './components/Table'
 import HelloWorld from './components/HelloWorld'
+import Form from './components/Form'
 
 class App extends Component {
   state = {
     characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
     ],
   }
   removeCharacter = (index) => {
-    const {characters} = this.state;
-     //filtering
-     const filteredCharacters = characters.filter((character, i) => {
-        return i !== index;
-     });
-     //setting state back for characters
-     this.setState({characters: filteredCharacters});
+    const { characters } = this.state;
+    //filtering
+    const filteredCharacters = characters.filter((character, i) => {
+      return i !== index;
+    });
+    //setting state back for characters
+    this.setState({ characters: filteredCharacters });
+  }
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] })
   }
   render() {
-    const {characters} = this.state;
+    const { characters } = this.state;
     return (
       <div>
         <header></header>
         <main>
           <HelloWorld></HelloWorld>
           <div className="container">
-            <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+            <Form handleSubmit={this.handleSubmit}></Form>
+            <Table characterData={characters} removeCharacter={this.removeCharacter} />
           </div>
         </main>
         <footer></footer>
